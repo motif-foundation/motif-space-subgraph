e07e09cdce5a8fc85dbfeb0bedd1bc23107b2cd8
Steve <98256181+johnwwk@users.noreply.github.com>
2022-09-17 09:42:55 +0300

    update

--- src/helpers.ts
+++ src/helpers.ts
@@ -502,11 +502,11 @@ export function createReserveListing(
     reserveListing.tokenContract = tokenContract 
     reserveListing.token = tokenContract.concat('-').concat(tokenId.toString())
     reserveListing.space = space ? space.id : null
-    reserveListing.approved = false
+    reserveListing.approved = true//false
     reserveListing.startsAt = startsAt
     reserveListing.duration = duration
     reserveListing.firstBidTime = BigInt.fromI32(0)
-    reserveListing.approvedTimestamp = null
+    reserveListing.approvedTimestamp = createdAtTimestamp//null
     reserveListing.listPrice = listPrice
     reserveListing.listType = listType 
     reserveListing.intermediaryFeePercentage = intermediaryFeePercentage
